fcd4512a0df2dfa65ceaa24cb40b66d248748db4
sayali-koli <kolisayali22@gmail.com>
2026-07-28 10:59:44 +0530

    Add Elif Statement

---
+++ 3.conditional _statement/2. elif_statement.py
@@ -0,0 +1,23 @@
+# sub1 = int(input("enter your maths marks : "))
+# sub2 = int(input("enter yor english marks : "))
+# sub3 = int(input("enter yor marathi marks : "))
+# sub4 = int(input("enter yor hindi marks : "))
+
+# total = sub1 + sub2 + sub3 + sub4
+
+# if total >= 91 and total <=100:
+#     print("grade A")
+# elif total >= 81 and total <= 90:
+#     print("grade B")
+# elif total >= 71 and total <= 80:
+#     print("grade C")
+# elif total >= 61 and total <= 70:
+#     print("grade D")
+# elif total >= 0 and total <= 60:
+#     print("fail")
+# else:
+#     print("invaild marks")
+
+
+
+
