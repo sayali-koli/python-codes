032a34e6e5dd455a810232383b29e278022f0e87
sayali-koli <kolisayali22@gmail.com>
2026-07-29 11:20:29 +0530

    Add Nested if else

---
+++ 3.conditional _statement/3. nested_if_else.py
@@ -0,0 +1,10 @@
+# age = int(input("enter you age : "))
+# certificate = input("enter your certificate : ")
+
+# if age >= 18:
+#     if certificate == "yes":
+#         print("you are hired")
+#     else:
+#         print("don't have certificate not hired")
+# else:
+#     print("you not hired because your age is less than 18")
