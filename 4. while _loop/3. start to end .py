8833a8e54701bba4fc961162b472302bc13940cb
sayali-koli <kolisayali22@gmail.com>
2026-07-30 17:07:00 +0530

    Add Start To End

---
+++ 4. while _loop/3. start to end .py
@@ -0,0 +1,18 @@
+# start = int(input("enter start number = "))
+# end = int(input("enter end number = "))
+
+# i = start
+# while i <= end:
+#     print(i)
+#     i += 1
+# print(f"after the while loop,start value is{start}")
+
+
+# start = int(input("enter your number : "))
+# end = int(input("enter your number : "))
+
+# i = start
+# while i <= end:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
