c11ca0150d6e6b0359c3ad8805ca295c12db286f
sayali-koli <kolisayali22@gmail.com>
2026-08-01 10:25:29 +0530

    Add Practice Questions On While Loop

---
+++ 4. while _loop/practice.py
@@ -0,0 +1,35 @@
+# i = 1
+# while i <= 100:
+#     if i % 3 == 0 and i % 5 == 0:
+#         print(i)
+#     i += 1
+
+# sum = 0
+# i = 1
+# while i <= 100:
+#     sum = sum + i
+#     i += 1
+# print("sum=", sum)
+
+
+# sum = 0
+# i = 1
+# while i <= 100:
+#     if i % 2 == 0 and i % 7 == 0:
+#         sum = sum + i
+#     i += 1
+# print("sum=", sum)
+
+
+# num = int(input("enter your number : "))
+# i = 1
+# while i <= 10:
+#     print(num * i)
+#     i += 1
+
+num = int(input("enter your number : "))
+i = 1
+while i <= 10:
+    if num % i == 0:
+        print(i)
+    i += 1
