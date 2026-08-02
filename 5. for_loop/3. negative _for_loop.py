dd25e3dd712ae154b0b44325e6f67aee609f8476
sayali-koli <kolisayali22@gmail.com>
2026-08-02 13:27:20 +0530

    Add Negative For Loop

---
+++ 5. for_loop/3. negative _for_loop.py
@@ -0,0 +1,11 @@
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+# for i in range(1, -11, -1):
+#     print(i, end=" ")
+
+
+for i in range(100, 0, -1):
+    if i % 2 == 0 and i % 3 == 0:
+        print(i, end=" ")
