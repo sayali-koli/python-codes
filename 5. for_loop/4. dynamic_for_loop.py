96927fb6c095edd3506ef6b4cf15db50c5cc9a54
sayali-koli <kolisayali22@gmail.com>
2026-08-03 11:19:35 +0530

    Add Dynamic For Loop

---
+++ 5. for_loop/4. dynamic_for_loop.py
@@ -0,0 +1,7 @@
+# start = int(input("enter your number : "))
+# end = int(input("enter your number : "))
+# steps = int(input("enter your steps : "))
+# total = 0
+# for i in range(start, end + 1, steps):
+#     total += i
+# print(total)
