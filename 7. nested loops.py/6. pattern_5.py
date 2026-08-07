fc74e1e84e4c022240728a384117b763b0e2197d
sayali-koli <kolisayali22@gmail.com>
2026-08-07 10:55:22 +0530

    Add Pattern 5

---
+++ 7. nested loops.py/6. pattern_5.py
@@ -0,0 +1,32 @@
+"""
+1
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+"""
+
+# for i in range(1, 6):
+#     for j in range(i, 0, -1):
+#         print(j, end=" ")
+#     print()
+
+
+"""
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+"""
+
+# for i in range(5, 0, -1):
+#     for j in range(i, 0, -1):
+#         print(j, end=" ")
+#     print()
+
+
+# for i in range(5, 0, -1):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
