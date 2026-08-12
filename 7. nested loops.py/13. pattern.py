6a26ac290e8a6da1609a7217f9597bd84e07069e
sayali-koli <kolisayali22@gmail.com>
2026-08-12 10:58:09 +0530

    Add Pattern 12

---
+++ 7. nested loops.py/13. pattern.py
@@ -0,0 +1,13 @@
+for i in range(1, 6):
+    for k in range(1, 6 - i):
+        print(" ", end=" ")
+    for j in range(1, i * 2):
+        print(j, end=" ")
+    print()
+
+for i in range(4, 0, -1):
+    for k in range(1, 5 - i + 1):
+        print(" ", end=" ")
+    for j in range(1, i * 2):
+        print(j, end=" ")
+    print()
