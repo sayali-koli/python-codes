81315355e9141066478bf40b4d0201db17eeab8e
sayali-koli <kolisayali22@gmail.com>
2026-08-14 17:35:21 +0530

    Add Parameter And Argument

---
+++ 8. function/2. parameter and argument.py
@@ -0,0 +1,13 @@
+# def addition(a, b, c):
+#     sum = a + b + c
+#     print(f"total={sum}")
+
+
+# addition(10, 20, 30)
+
+
+# def info(name, age, gender):
+#     print(f"your name is{name},your age is{age}and your gender is{gender}")
+
+
+# info("sayali", 22, "femal")
