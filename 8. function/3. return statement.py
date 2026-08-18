919f5f1edcf6494b88c105bfe05949019e26ef7d
sayali-koli <kolisayali22@gmail.com>
2026-08-18 17:06:57 +0530

    Add  Return Statement

---
+++ 8. function/3. return statement.py
@@ -0,0 +1,16 @@
+# def addation(n1, n2, n3):
+#     return n1 + n2 + n3
+
+
+# sum = addation(1, 2, 3)
+# print(sum)
+
+
+def can_vote(age):
+    if age >= 18:
+        return True
+    else:
+        return False
+
+
+print(can_vote(20))
