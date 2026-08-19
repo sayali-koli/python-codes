674d56890852f3a02b2bc7ceda869de2a5f3f351
sayali-koli <kolisayali22@gmail.com>
2026-08-19 18:57:13 +0530

    Add Practice

---
+++ 8. function/3.4.1. practice.py
@@ -0,0 +1,28 @@
+# def square(num):
+#     return num**2
+
+
+# print(square(4))
+
+
+# def min_of_three(n1, n2, n3):
+#     if n1 < n2 and n1 < n3:
+#         return f"{n1} is smallest"
+#     elif n2 < n1 and n2 < n3:
+#         return f"{n2} is smallest"
+#     return f"{n3} is smallest"
+
+
+# print(min_of_three(1, 2, 3))
+
+
+# def adsolute_value(num):
+
+#     if num >= 0:
+#         return num
+#     return num * -1
+
+
+# print(adsolute_value(-100))
+
+
