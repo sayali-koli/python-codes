6db8f437aafe163825972db60a3e15c7473c1c2b
sayali-koli <kolisayali22@gmail.com>
2026-08-24 21:36:58 +0530

    Add Lambda Function

---
+++ 8. function/10. lambda_function.py
@@ -0,0 +1,13 @@
+# square = lambda num: num * num
+
+# print(square(100))
+
+
+# applicable = lambda age: age >= 18
+
+# print(applicable(10))
+
+
+applicable = lambda age: "valid" if age >= 18 else "invalid"
+
+print(applicable(10))
