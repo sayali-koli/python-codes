a42d5e1a27f0be2349028952c62319dca53e6088
sayali-koli <kolisayali22@gmail.com>
2026-08-30 09:44:56 +0530

    Add List Index Practice

---
+++ 9. list _in_python/5. list_index_practice.py
@@ -0,0 +1,23 @@
+movie = ["sairat", "raja shivaji", "robot", "lai bhari", "vivha", "bala"]
+print(len(movie))
+print(movie[0])
+print(movie[2])
+print(movie[-1])
+print(
+    f"first movie is:{movie[0]},second movie is:{movie[2]},third movie is:{movie[-1]}"
+)
+
+
+lst = [10, 20, 30, 40, 50]
+lst[1] = 0
+lst[3] = 0
+print(lst)
+
+
+
+
+lst = [56, 34, 25, 40, 70, 77, 17, 59, 60, 66, 47]
+n = len(lst)
+print(lst[0])
+print(lst[n - 1])
+print(lst[n // 2])
