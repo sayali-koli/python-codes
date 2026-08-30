c28532b4f56513ba95fc30c1d2e173e7222942ff
sayali-koli <kolisayali22@gmail.com>
2026-08-30 09:45:50 +0530

    Add Sclicing

---
+++ 9. list _in_python/6.sclicing.py
@@ -0,0 +1,19 @@
+lst = [56, 34, 25, 40, 70, 77, 17, 59, 60, 66, 47]
+
+n = len(lst)
+print(lst[1:7])
+print(lst[0:6])
+print(lst[0:])
+print(lst[:11])
+print(lst[0:8:1])
+print(lst[0:11:1])
+print(lst[0:5:2])
+print(lst[:11:2])
+print(lst[0::3])
+print(lst[::4])
+
+
+print(lst[9:1:-1])
+print(lst[11:1:-1])
+print(lst[::-1])
+print(lst[::-2])
