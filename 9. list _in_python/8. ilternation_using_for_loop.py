536368e68971920575628161f7ff53311357b88e
sayali-koli <kolisayali22@gmail.com>
2026-09-03 21:52:16 +0530

    Add Ilternation Using For Loop

---
+++ 9. list _in_python/8. ilternation_using_for_loop.py
@@ -0,0 +1,24 @@
+nums = [56, 34, 25, 40, 70, 77, 17, 59, 60, 66, 47]
+# n = len(num)
+# sum = 0
+# for i in range(0, 11):
+#     sum = sum + num[i]
+#     # print(num[i], end=" ")
+# print(sum)
+
+
+total = 0
+for num in nums:
+    total = total + num
+print(total)
+
+
+for num in nums[::-1]:
+    print(num)
+
+
+# num = [56, 34, 25, 40, 70, 77, 17, 59, 60, 66, 47]
+# n = len(num)
+
+# for i in range(10, 0, -1):
+#     print(num[i], end=" ")
