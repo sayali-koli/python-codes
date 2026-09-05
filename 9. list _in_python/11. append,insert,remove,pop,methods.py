81b98b5adf85086b2f8edde482379316e85de9df
sayali-koli <kolisayali22@gmail.com>
2026-09-05 12:13:43 +0530

    Add Append Insert Remove Pop Methods

---
+++ 9. list _in_python/11. append,insert,remove,pop,methods.py
@@ -0,0 +1,26 @@
+# lst = ["sayali", 12.4, 222, True, "solapur"]
+# print(lst)
+# lst.append("shubhangi")
+# lst.append(13.4)
+# lst.append("police")
+# print(lst)
+
+
+# lst = ["sayali", 12.4, 222, True, "solapur"]
+# lst.insert(1, "shravan")
+# lst.insert(3, False)
+# print(lst)
+
+
+# lst = ["sayali", 12.4, 222, True, "solapur"]
+# # lst.pop()
+# # lst.pop(0)
+# print(lst)
+
+# x = lst.pop()
+# print(x)
+
+
+# lst = ["sayali", 12.4, 222, True, "solapur"]
+# lst.remove(222)
+# print(lst)
