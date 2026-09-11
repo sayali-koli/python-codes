efa734c9c2a05b7fe61cd9bef8ecfbd16e35739c
sayali-koli <kolisayali22@gmail.com>
2026-09-11 22:08:03 +0530

    Add Dynamic For Iteration

---
+++ 9. list _in_python/17. dynamic_for_iteration.py
@@ -0,0 +1,14 @@
+# matrix = [
+#     [5, 7, 2, 3, 1],
+#     [6, 3, 8, 1, 3],
+#     [6, 4, 9, 0, 2],
+#     [3, 6, 3, 8, 1],
+# ]
+
+# row = len(matrix)
+# columns = len(matrix[0])
+
+# for i in range(0, row):
+#     for j in range(0, columns):
+#         print(matrix[i][j], end=" ")
+#     print()
