0c6d7ee019b9701dd690abe4d4040005d924d428
sayali-koli <kolisayali22@gmail.com>
2026-09-15 11:46:49 +0530

    Add Packaging And Unpackaging

---
+++ 10. tuples_in_python.py/5. packaging_and_unpackaging.py
@@ -0,0 +1,15 @@
+my_tuple = (1, 2, 3, 4)
+print(my_tuple)
+
+my_tuple = (1,)
+print(my_tuple)
+print(type(my_tuple))
+
+a, b, c, d = 1, 2, 3, 4
+print(a, b, c, d)
+
+a, b, c, d = 1, 2, 3, 4
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
