95b0a2cc8e65bb2eb33369640619f2ac4ee7f8af
sayali-koli <kolisayali22@gmail.com>
2026-09-16 14:22:24 +0530

    Add Use Case Of Unpacking

---
+++ 10. tuples_in_python.py/6. use case of unpacking.py
@@ -0,0 +1,26 @@
+# def min_max(lst):
+#     # maxi=float("-inf")
+#     for maxi in lst:
+#         maxi = max(lst)
+
+#     for mini in lst:
+#         mini = min(lst)
+#         return maxi, mini
+
+
+# my_list = (10, 20, 30, 40, 100)
+# print(min_max(my_list))
+
+
+# def mini_maxi(tup):
+#     maxi = max(tup)
+#     mini = min(tup)
+
+#     return maxi, mini
+
+
+# mytuple = (1, 2, 4, 5, 7, 9, 100)
+# # print(mini_maxi(mytuple))
+# ans1, ans2 = mini_maxi(mytuple)
+# print(f"maximum={ans1}")
+# print(f"minimum={ans2}")
