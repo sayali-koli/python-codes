564af31c95b15acfc283dd3f90528101ca7bd3c4
sayali-koli <kolisayali22@gmail.com>
2026-09-19 22:04:02 +0530

    Add Basic of Dictionary

---
+++ 11. dictionary_in_python/1. basic_.py
@@ -0,0 +1,12 @@
+marks = {
+    "science": 80,
+    "maths": 90,
+    "comp": 95,
+    55: 100,
+    101: "sayali",
+    "abc": [1, 2, 3, 4],
+    (11, 12): "solapur",
+}
+
+print(type(marks))
+print(marks)
