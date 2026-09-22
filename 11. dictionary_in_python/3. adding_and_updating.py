b43470f4f8f17aa888197511f521cfeab02c44a1
sayali-koli <kolisayali22@gmail.com>
2026-09-22 11:07:25 +0530

    Add Adding And Updating

---
+++ 11. dictionary_in_python/3. adding_and_updating.py
@@ -0,0 +1,11 @@
+student = {"name": "sayali", "age": 22}
+student["age"] = 23
+student["gender"] = "femal"
+student.update(
+    {
+        "city": "solapur",
+        "father name": "jagdish koli",
+        "phone no": 8308259016,
+    }
+)
+print(student)
