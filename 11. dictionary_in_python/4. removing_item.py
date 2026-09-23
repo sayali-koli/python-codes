68748ae96e68b2fc974c8b327e12d521b44c43b2
sayali-koli <kolisayali22@gmail.com>
2026-09-23 11:07:30 +0530

    Add Removing Item

---
+++ 11. dictionary_in_python/4. removing_item.py
@@ -0,0 +1,18 @@
+student = {
+    "name": "sayali",
+    "age": 23,
+    "gender": "femal",
+    "city": "solapur",
+    "father name": "jagdish koli",
+    "phone no": 8308259016,
+}
+
+# student.pop("name")
+# print(student)
+
+# student.clear()
+# print(student)
+
+
+# del student["age"]
+# print(student)
